94b0217115e6e3f03e587252092a4f7ac179135a
drewlong314 <drewlong314@gmail.com>
2025-03-29 16:00:41 -0500

    created working stopwatch for seconds

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,45 @@
-import './App.css'
+import { useRef, useState } from "react";
+import "./App.css";
 
 function App() {
+  const [startTime, setStartTime] = useState(null);
+  const [now, setNow] = useState(null);
+  const [storedTime, setStoredTime] = useState(0);
+  const intervalRef = useRef(null);
+
+  let timePassed = 0;
+  if (startTime != null && now != null && storedTime === 0) {
+    timePassed = ((now - startTime) / 1000).toFixed(2);
+  } else if (startTime != null && now != null) {
+    timePassed = ((now - startTime) / 1000 + storedTime).toFixed(2);
+  } else timePassed = storedTime.toFixed(2);
+
+  const startTimer = () => {
+    setStartTime(Date.now());
+    setNow(Date.now());
+
+    intervalRef.current = setInterval(() => {
+      setNow(Date.now());
+    }, 10);
+  };
+
+  const stopTimer = () => {
+    if (!storedTime) setStoredTime((now - startTime) / 1000);
+    else setStoredTime((now - startTime) / 1000 + storedTime);
+    clearInterval(intervalRef.current);
+    intervalRef.current = null;
+    setStartTime(null);
+    setNow(null);
+  };
 
   return (
     <>
       <h1>Time Tracker</h1>
+      <div>{timePassed}</div>
+      <button onClick={startTimer}>start</button>
+      <button onClick={stopTimer}>stop</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
