053901d55b374c267987c02f9e4bd3b110bb8f6e
drewlong314 <drewlong314@gmail.com>
2025-04-01 21:23:14 -0500

    switched to typescript

--- src/components/timer.tsx
+++ src/components/timer.tsx
@@ -1,26 +1,27 @@
+import * as React from "react";
 import "./Timer.css";
 import { useRef, useState } from "react";
 
 function Timer() {
-  const [startTime, setStartTime] = useState(null);
-  const [now, setNow] = useState(null);
+  const [startTime, setStartTime] = useState<number | null>(null);
+  const [now, setNow] = useState<number | null>(null);
   const [storedTime, setStoredTime] = useState(0);
   const [isRunning, setIsRunning] = useState(false);
-  const intervalRef = useRef(null);
+  const intervalRef = useRef<NodeJS.Timeout | null>(null);
 
-  let timePassed = 0;
+  let timePassed: string = "";
   if (startTime != null && now != null && storedTime === 0) {
     timePassed = ((now - startTime) / 1000).toFixed(2);
   } else if (startTime != null && now != null) {
     timePassed = ((now - startTime) / 1000 + storedTime).toFixed(2);
   } else timePassed = storedTime.toFixed(2);
 
-  const formatNumber = (number) => {
+  const formatNumber = (number: number) => {
     return number.toString().padStart(2, "0");
   };
 
-  const formatTime = (timePassed) => {
-    const convertedTime = new Date(timePassed * 1000);
+  const formatTime = (timePassed: string) => {
+    const convertedTime = new Date(Number(timePassed) * 1000);
     const hours = convertedTime.getUTCHours();
     const minutes = convertedTime.getUTCMinutes();
     const seconds = convertedTime.getUTCSeconds();
@@ -40,6 +41,7 @@ function Timer() {
   };
 
   const clearTimer = () => {
+    //@ts-ignore
     clearInterval(intervalRef.current);
     intervalRef.current = null;
     setStartTime(null);
@@ -48,9 +50,13 @@ function Timer() {
   };
 
   const stopTimer = () => {
-    if (!storedTime) setStoredTime((now - startTime) / 1000);
-    else setStoredTime((now - startTime) / 1000 + storedTime);
-    clearTimer();
+    if (now !== null && startTime !== null) {
+      if (!storedTime) setStoredTime((now - startTime) / 1000);
+      else setStoredTime((now - startTime) / 1000 + storedTime);
+      clearTimer();
+    } else {
+      console.log("now are startTime are null")
+    }
   };
 
   const resetTimer = () => {
